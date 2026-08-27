1243dec33a8d32d892c8a5594bac6b39fc38dd93
Nia9-4 <areul@uni-osnabrueck.de>
2026-08-27 09:31:28 +0200

    additions to train.py and evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,20 +1,46 @@
 import warnings
 import torch
-from model import CNN
+from model import BaselineMLP, EEGClassifier
 
-model = CNN()
 
-model.load_state_dict(torch.load("checkpoint.pt"))
+"""TO DO:
+* check whether functions are still missing or everything works
+* do models really have to be loaded separately?
+* perform little test with small dataset
+"""
 
-model.eval()
+
+# Evaluate baseline model
+
+model_baseline = BaselineMLP(input_dim=, num_classes=) 
+model_baseline.load_state_dict(torch.load("checkpoint.pt"))
+model_baseline.eval()
 
 correct = 0
 
 with torch.no_grad():
     for x, y in test_loader:
-        output = model(x)
-        pred = output.argmax(dim=1)
-        correct += (pred==y).sum()
+        output_baseline = model_baseline(x)
+        pred_baseline = output_baseline.argmax(dim=1)
+        correct_baseline += (pred_baseline==y).sum()
 
-acc = correct / len(test_loader.dataset)
-print(acc)
+acc_baseline = correct_baseline / len(test_loader.dataset)
+print(f"Accuracy of the baseline MLP: {acc_baseline}")
+
+
+# Evaluate main model
+
+model_main = EEGClassifier() # add params here
+model_main.load_state_dict(torch.load("checkpoint.pt"))
+model_main.eval()
+
+correct = 0
+
+with torch.no_grad():
+    for x, y in test_loader:
+        output_main = model_main(x)
+        pred_main = output_main.argmax(dim=1)
+        correct_main += (pred_main==y).sum()
+
+acc_main = correct_main / len(test_loader.dataset)
+print(f"Accuracy of the main EEG Classifier (CNN-Transformer): {acc_main}")
--- src/train.py
+++ src/train.py
@@ -4,9 +4,15 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as Dataset, DataLoader
 
-from model import CNN
+from model import BaselineMLP, EEGClassifier
 from dataset import PreprocessedDataset, load_and_preprocess
 from datamodule import create_dataloaders
+
+
+""" TO DO:
+* ensure all necessary params given
+* training loop completeness and functionality
+"""
 
 
 # Device is assigned to cuda (GPU) if available
@@ -18,25 +24,39 @@ torch.backends.cuddn.deterministic = True
 preprocessor = PreprocessedDataset()
 X, y = preprocessor.load_and_preprocess()
 
-# model = EEGClassifier(self, patch_size=32, in_channels=32, embed_dim=128, 
+model_baseline = BaselineMLP(input_dim=, num_classes=) 
+# hidden_dim and dropout could also be modified if desired
+model_main = EEGClassifier()
+# add the params for the model: (patch_size=32, in_channels=32, embed_dim=128, 
 # num_patches = 10, out_channels_cnn=64, transfomer_layers=2, num_heads=4, 
 # ff_dim=256, expansion = ?, dropout_rate=0.1)
 
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer_main = optim.Adam(model_main.parameters(), lr=0.001)
+optimizer_baseline = optim.Adam(model_baseline.parameters(), lr=0.001)
 
 # Create train and test dataloaders
 train_loader, test_loader = create_dataloaders(X, y)
 
-# Training loop
-
+# Training loop for main model
 num_epochs = 8 # number of epochs
 
 for epoch in range(num_epochs):
     for batch in dataloader:
-        #train(model, train_loader)
-        #optimizer.zero_grad()
-        #pred = model(x)
-        #loss = nn.CrossEntropyLoss(pred, x)
-        #loss.backward()
-        optimizer.step()
+        train(model_main, train_loader)
+        optimizer_main.zero_grad()
+        pred = model_main(x)
+        loss = nn.CrossEntropyLoss(pred, x)
+        loss.backward()
+        optimizer_main.step()
     print(loss.item())
+
+# Training loop for baseline model
+for epoch in range(num_epochs):
+    for batch in dataloader:
+        train(model_baseline, train_loader)
+        optimizer_baseline.zero_grad()
+        pred = model_baseline(x)
+        loss = nn.CrossEntropyLoss(pred, x)
+        loss.backward()
+        optimizer_baseline.step()
+    print(loss.item)
