e350777264be1b1f1abf04bdfeae7ecf09047fef
Antonia <antonia.reul@web.de>
2026-09-15 20:33:30 +0100

    add job.sh

--- job.sh
+++ job.sh
@@ -3,18 +3,17 @@
 #SBATCH --time=12:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
 #SBATCH --mem=32G
 #SBATCH --output=logs/result_%j.out
-#SBATCH --error=logs/result_j%.err
+#SBATCH --error=logs/result_%j.err
 
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=areul@uos.de
+#SBATCH --mail-user=areul@uni-osnabrueck.de
 
-wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x84_64.sh -b -p "miniconda" -u
-source miniconda/bin/activate
-conda create -y -q --name dl26 python=3.10 -- channel defaults
+module load cuda/11.8
 source activate dl26
-pip install --no-cache-dir --upgrade -r requirements.txt
 
-srun python src/train.py
+python -u src/train.py
+
